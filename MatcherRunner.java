e0582c67ef970507c402ddd321ec6c673890874b
Jackatk2005 <Jack-Becker@comcast.net>
2021-11-30 15:42:42 -0600

    Fixed Inf loop

--- MatcherRunner.java
+++ MatcherRunner.java
@@ -1,5 +1,3 @@
-import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
-
 import java.util.Scanner;
 
 public class MatcherRunner {
@@ -33,22 +31,27 @@ public class MatcherRunner {
     public static void CommandProcessor() {
         Scanner input = new Scanner(System.in);
         System.out.println("Attempt to Match These Base 10 Numbers With a Random Base Between 1 and 10");
-        System.out.println("Would You Like to Play on:" + "\n" + "Easy" + "\n" + "Medium" + "\n" + "Or Hard");
-        String difficulty = input.nextLine();
 
-        DifficultyChecker DC = new DifficultyChecker(difficulty);
+        while(true) {
+            System.out.println("Would You Like to Play on:" + "\n" + "Easy" + "\n" + "Medium" + "\n" + "Or Hard");
+            String difficulty = input.nextLine();
+            DifficultyChecker DC = new DifficultyChecker(difficulty);
             int i = DC.check();
-                if (i == 1) {
-                    System.out.println("Easy It Is");
-                    d = 1;
-                } else if (i == 2) {
-                    System.out.println("Medium It Is");
-                    d = 2;
-                } else if (i == 3) {
-                    System.out.println("Hard It Is");
-                    d = 3;
-                }
-
-        Difficulty df = new Difficulty(d);
+            if (i == 1) {
+                System.out.println("Easy It Is");
+                d = 1;
+                break;
+            } else if (i == 2) {
+                System.out.println("Medium It Is");
+                d = 2;
+                break;
+            } else if (i == 3) {
+                System.out.println("Hard It Is");
+                d = 3;
+                break;
+            } else if (i == 4) {
+                System.out.println("Invalid Input Try Again");
+            }
+        }
     }
 }

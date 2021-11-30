34266ed6f337b518ff695e2e66c8271c52fb3b30
Jackatk2005 <Jack-Becker@comcast.net>
2021-11-29 21:23:15 -0600

    11/29/21

---
+++ MatcherRunner.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class MatcherRunner
+{
+    public static void main(String[] args)
+    {
+        //Declare Scanner
+        //{
+        Scanner input = new Scanner(System.in);
+        //}
+        
+        //Startup Messages
+        //{
+        System.out.println("Hello User");    
+        System.out.println("What shall I Call You");    
+        String name = input.nextLine();
+        System.out.println("Ok " + name + " It's Nice to Meet You");
+        //}
+        
+        //While loop in order to quit or send to process method
+        //{
+        while(true)
+        {
+            System.out.println("Would You Like to Play My Game or Quit  (Enter: play/quit)");
+            String command = input.nextLine();
+            if(command.equalsIgnoreCase("quit"))
+            {
+                System.out.println("Goodbye");
+                input.close();
+                break;
+            }
+            else if(command.equalsIgnoreCase("play"))
+            {
+                CommandProcesser();
+            }
+        }    
+        //}
+        }
+    
+    public static void CommandProcesser(){
+        System.out.println("Hello World");
+        Scanner input = new Scanner(System.in);
+        
+
+    }
+    }

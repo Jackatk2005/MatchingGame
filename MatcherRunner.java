8ee4c9eb6bcb412c6dc08d023264e8af815f047e
Jackatk2005 <Jack-Becker@comcast.net>
2021-11-29 22:02:35 -0600

    11/29/21

--- MatcherRunner.java
+++ MatcherRunner.java
@@ -5,20 +5,15 @@ public class MatcherRunner
     public static void main(String[] args)
     {
         //Declare Scanner
-        //{
         Scanner input = new Scanner(System.in);
-        //}
         
         //Startup Messages
-        //{
         System.out.println("Hello User");    
         System.out.println("What shall I Call You");    
         String name = input.nextLine();
         System.out.println("Ok " + name + " It's Nice to Meet You");
-        //}
         
         //While loop in order to quit or send to process method
-        //{
         while(true)
         {
             System.out.println("Would You Like to Play My Game or Quit  (Enter: play/quit)");
@@ -34,13 +29,12 @@ public class MatcherRunner
                 CommandProcesser();
             }
         }    
-        //}
-        }
+    }
     
     public static void CommandProcesser(){
-        System.out.println("Hello World");
+        System.out.println("Attempt to Match These Base 10 Numbers With a Random Base Between 1 and 10");
         Scanner input = new Scanner(System.in);
-        System.out.println("Hello");
+        
 
     }
-    }
+}
